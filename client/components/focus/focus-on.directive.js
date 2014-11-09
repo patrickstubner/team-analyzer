42c9415ceadaa7f34b001ebc709593034c6a7fb3
patrickstubner <pstubner@gmail.com>
2014-11-10 00:57:47 +0100

    tooltip and time fix

--- client/components/focus/focus-on.directive.js
+++ client/components/focus/focus-on.directive.js
@@ -10,3 +10,14 @@ angular.module('teamAnalyzerApp')
         });
      };
   });
+
+angular.module('teamAnalyzerApp')
+  .directive('tooltip', function() {
+  return {
+    restrict: 'EA',
+    link: function(scope, element, attrs) {
+      attrs.tooltipTrigger = attrs.tooltipTrigger;
+      attrs.tooltipPlacement = attrs.tooltipPlacement || 'top';
+    }
+  }
+});
